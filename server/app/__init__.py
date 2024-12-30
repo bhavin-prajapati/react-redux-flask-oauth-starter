a45344e84e46c51a6802b6e13bcc5db1a2a3658a
Bhavin Prajapati <bhavin.prajapati@gmail.com>
2024-12-30 01:50:10 -0500

    Working on Facebook auth flow

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -4,6 +4,7 @@ import base64
 import redis
 from flask import Flask, make_response, send_from_directory, redirect, url_for
 from flask import render_template, request, url_for, redirect
+from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 from flask_dance.contrib.google import make_google_blueprint, google
 from flask_dance.contrib.github import make_github_blueprint, github
 from flask_cors import CORS, cross_origin
@@ -19,6 +20,10 @@ app.json.ensure_ascii = False
 
 # Connect to Redis
 r = redis.Redis(host='localhost', port=6379, db=0, protocol=3)
+
+# Register the Facebook OAuth BP built by Flask-Dance 
+facebook_bp = make_facebook_blueprint(scope='email,public_profile,user_photos')
+app.register_blueprint(facebook_bp, url_prefix="/login")
 
 # Register the Google OAuth BP built by Flask-Dance 
 google_bp = make_google_blueprint(scope=["https://www.googleapis.com/auth/userinfo.email", 
@@ -38,13 +43,21 @@ def privacy_policy():
 def terms_of_service():
     return "<p>Terms Of Service</p>"
 
-@app.route("/data-deletion")
+@app.route('/data-deletion', methods=['POST'])
 def data_deletion():
-    return "<p>data-Data Deletion</p>"
+    # Extract user ID from request payload
+    data = json.loads(request.data)
+    user_id = data.get('user_id')
+    # Perform data deletion logic here
+    # ...
+    # Return a confirmation response
+    return json.dumps({'success': True}), 200
 
 @app.route("/")
 def Hello():
-    if google.authorized:
+    if facebook.authorized:
+        response = make_response(redirect('/api/v1/login-facebook'))
+    elif google.authorized:
         response = make_response(redirect('/api/v1/login-google'))
     elif github.authorized:
         response = make_response(redirect('/api/v1/login-github'))
@@ -66,11 +79,32 @@ def counter():
         r.set('count', int(count) + 1)
     return "<p>Counter: " + str(count) + " </p>"
 
+@app.route('/api/v1/login-facebook')
+@cross_origin()
+def login_facebook():
+    """Log in a registered Facebook User."""
+    user=None
+    if not facebook.authorized:
+        return redirect(url_for('facebook.login'))
+    else:
+        resp = facebook.get('/me')
+        assert resp.ok
+        print(resp.json())
+        name=resp.json()["name"]
+        email=resp.json()["email"]
+        avatar_url=resp.json()["avatar_url"]
+        user={"username":str(username),"name":str(name),"email": str(email),"avatar_url": str(avatar_url)}
+    response = make_response(redirect(request.referrer))
+    user_json = json.dumps(user, ensure_ascii=False)
+    user_json_b64 = base64.b64encode(bytes(user_json, 'utf-8'))
+    user_json_b64_str = str(user_json_b64)[1:]
+    response.set_cookie("user", value=user_json_b64_str, max_age=None, expires=None, path='/', secure=None, httponly=False)
+    return response
+
 @app.route('/api/v1/login-google')
 @cross_origin()
 def login_google():
     """Log in a registered Google User."""
-    username=None
     user=None
     if not google.authorized:
         print('not google.authorized')
@@ -119,3 +153,4 @@ def logout():
     response.set_cookie("session", '', expires=0)
     response.set_cookie("user", '', expires=0)
     return response
+
--- server/app/config.py
+++ server/app/config.py
@@ -12,6 +12,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
     # Set up the App SECRET_KEY
     SECRET_KEY = config('SECRET_KEY', default='tHe#$uP3R$3cR3tK3y')
+    FACEBOOK_OAUTH_CLIENT_ID     = config('FACEBOOK_OAUTH_CLIENT_ID')
+    FACEBOOK_OAUTH_CLIENT_SECRET = config('FACEBOOK_OAUTH_CLIENT_SECRET')
     GITHUB_OAUTH_CLIENT_ID     = config('GITHUB_OAUTH_CLIENT_ID')
     GITHUB_OAUTH_CLIENT_SECRET = config('GITHUB_OAUTH_CLIENT_SECRET')
     GOOGLE_OAUTH_CLIENT_ID     = config('GOOGLE_OAUTH_CLIENT_ID')
